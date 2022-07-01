993ca01d676791247c9683938a51997b18857bf9
Berzan <berzan@tutanota.com>
2022-07-01 21:47:30 +0300

    change: test script

--- tests/gif-portal-contract.ts
+++ tests/gif-portal-contract.ts
@@ -1,16 +1,16 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { GifPortalContract } from "../target/types/gif_portal_contract";
 
-describe("gif-portal-contract", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
-  const program = anchor.workspace.GifPortalContract as Program<GifPortalContract>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+try {
+  (async () => {
+    console.log("🚀 Starting test...")
+  
+    anchor.setProvider(anchor.AnchorProvider.env())
+    const program = anchor.workspace.GifPortalContract
+    const tx = await program.rpc.initialize()
+  
+    console.log("Your tx id:", tx)
+    process.exit(0)
+  })()
+} catch (error) {
+  console.error(error);
+}
